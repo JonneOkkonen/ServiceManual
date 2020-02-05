f7d4ddce008d0ef3e6388ffdd4c09a3d92454e91
Jonne Okkonen <okkonen.jonne@gmail.com>
2020-02-06 00:29:32 +0200

    UnitTests: MaintenanceTaskExists Test
    Test if MaintenanceTaskExists method works as intended
    Fix MaintenanceTaskExists method. RecordsAffected can't be used with
    SELECT.

--- ServiceManualUnitTests/Database.cs
+++ ServiceManualUnitTests/Database.cs
@@ -1,13 +1,23 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceManual;
 
 namespace ServiceManualUnitTests
 {
     [TestClass]
-    public class UnitTest1
+    public class DatabaseTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void MaintenanceTaskExist()
         {
+            Database db = new Database();
+
+            // Check true output
+            bool actual = db.MaintenanceTaskExists(1);
+            Assert.AreEqual(true, actual);
+
+            // Check false output
+            bool actual2 = db.MaintenanceTaskExists(9999);
+            Assert.AreEqual(false, actual2);
         }
     }
 }
