94e1dfebe9c2fc043346e850633bb2de57e1d82c
Jonne Okkonen <okkonen.jonne@gmail.com>
2021-02-09 21:01:27 +0200

    Change route naming
    Change route naming for less confusing
    Example change /tasks to /task/all

--- ServiceManual/Controllers/v1/ApiRoute.cs
+++ ServiceManual/Controllers/v1/ApiRoute.cs
@@ -9,17 +9,17 @@ namespace ServiceManual.Controllers.v1
 
         public static class Tasks
         {
-            public const string GetAll = Base + "/tasks";
-            public const string Create = Base + "/tasks";
+            public const string GetAll = Base + "/task/all";
+            public const string Create = Base + "/task";
             public const string Get = Base + "/task/{taskID}";
             public const string Update = Base + "/task/{taskID}";
             public const string Delete = Base + "/task/{taskID}";
-            public const string GetAllForDevice = Base + "/tasks/{deviceID}";
+            public const string GetAllForDevice = Base + "/task/{deviceID}";
         }
 
         public static class Devices
         {
-            public const string GetAll = Base + "/devices";
+            public const string GetAll = Base + "/device/all";
             public const string GetOne = Base + "/device/{deviceID}";
         }
     }
